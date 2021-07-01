2aae3dc171170def22e81c2921c60eace94f9505
Jimmy Zheng <jimmy_842@berkeley.edu>
2021-07-01 16:22:35 -0700

    added sse functionality to index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ const fs = require('fs');
 const path = require('path');
 const router = express.Router();
 
+const hiPayload = {
+  key: "hi",
+  value: "false"
+}
+
 /*
 Note that this route should not be available unless someone
 requests with a valid key for security reasons.
@@ -10,6 +15,56 @@ requests with a valid key for security reasons.
 const validKey = (authHeader) => {
   const dummyAuthenticator = "JazzyElksRule"
   return authHeader === dummyAuthenticator;
+}
+
+function eventsHandler(request, response, next) {
+  const authHeader = request.get("Authorization");
+
+  if (!validKey(authHeader)) {
+    return res.status(500).send({
+      message: 'Invalid authentication key'
+    });
+  }
+  const headers = {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache'
+  };
+  console.log(request.get("Authorization"));
+  response.writeHead(200, headers);
+
+  // const data = `data: ${JSON.stringify(facts)}\n\n`;
+
+  // response.write(data);
+
+  const clientId = Date.now();
+
+  const newClient = {
+    id: clientId,
+    response
+  };
+
+  clients.push(newClient);
+
+  request.on('close', () => {
+    console.log(`${clientId} Connection closed`);
+    clients = clients.filter(client => client.id !== clientId);
+  });
+
+  const init = {
+    eventType: "FEATURE_UPDATES",
+    payload: hiPayload
+  }
+
+  newClient.response.write(`data: ${JSON.stringify(init)}\n\n`)
+}
+
+function sendEventsToAll(payload) {
+  const data = {
+    eventType: "FEATURE_UPDATE",
+    payload
+  }
+  clients.forEach(client => client.response.write(`data: ${JSON.stringify(data)}\n\n`))
 }
 
 router.get('/ruleset', function (req, res, next) {
@@ -27,6 +82,18 @@ router.get('/ruleset', function (req, res, next) {
   // in the lib dir.
   res.sendFile(path.join(__dirname, '../lib', '/ruleset.json'));
 });
+
+router.get('/features', eventsHandler);
+
+router.put('/features/hi', function(req, res, next) {
+  hiPayload.value = !hiPayload.value;
+  sendEventsToAll(hiPayload);
+
+  res.write(`sent ${hiPayload.value}`);
+  res.end();
+})
+
+let clients = [];
 
 module.exports = router;
 
